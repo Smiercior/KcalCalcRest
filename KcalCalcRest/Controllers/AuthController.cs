642ba55896fa1146602dcc56011530a0497c069f
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-06-13 15:21:36 +0200

    Update AuthController.cs

--- KcalCalcRest/Controllers/AuthController.cs
+++ KcalCalcRest/Controllers/AuthController.cs
@@ -5,14 +5,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace KcalCalcRest.Controllers; 
 
-[Route("api/register")]
+[Route("api/auth")]
 [ApiController]
 public class AuthController : BaseApiController {
 	public AuthController(IRepositoryManager repository, IMapper mapper) : base(repository, mapper) { }
 
-	[HttpPost]
+	[HttpPost("register")]
 	public async Task<IActionResult> RegisterUser([FromBody] UserRegistrationDTO userRegistration) {
 		var userResult = await _repository.UserAuthentication.RegisterUserAsync(userRegistration);
 		return !userResult.Succeeded ? new BadRequestObjectResult(userResult) : StatusCode(201);
+	}
+
+	[HttpPost("login")]
+	public async Task<IActionResult> Authenticate([FromBody] UserLoginDTO userLogin) {
+		return !await _repository.UserAuthentication.ValidateUserAsync(userLogin) 
+			? Unauthorized() 
+			: Ok(new { Token = await _repository.UserAuthentication.CreateTokenAsync() });
 	}
 }
