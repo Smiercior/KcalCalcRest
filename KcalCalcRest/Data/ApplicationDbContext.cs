91eb95916aad8a2936879a329352243d06e5869c
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-06-13 11:01:27 +0200

    Add Identity.EntityFrameworkCore

--- KcalCalcRest/Data/ApplicationDbContext.cs
+++ KcalCalcRest/Data/ApplicationDbContext.cs
@@ -1,17 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using KcalCalcRest.Models;
 
-namespace KcalCalcRest.Data; 
-    public class ApplicationDbContext : DbContext {
+namespace KcalCalcRest.Data;
+public class ApplicationDbContext : DbContext {
 	public DbSet<User> Users { get; set; }
 	public DbSet<Product> Products { get; set; }
 	public DbSet<ProductEntry> ProductEntries { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
-        }
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+	}
 
-        protected override void OnModelCreating(ModelBuilder builder){
-            base.OnModelCreating(builder);
+	protected override void OnModelCreating(ModelBuilder builder) {
+		base.OnModelCreating(builder);
 
 		builder.Entity<ProductEntry>()
 			.HasOne(productEntry => productEntry.User)
@@ -24,5 +24,5 @@ namespace KcalCalcRest.Data;
 			.WithMany(product => product.ProductEntries)
 			.HasForeignKey(productEntry => productEntry.ProductId)
 			.OnDelete(DeleteBehavior.Cascade);
-        }
-    }
+	}
+}
