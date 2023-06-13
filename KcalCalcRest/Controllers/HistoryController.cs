0e8e292c17f06916004a2408cd7b188ff0a792f9
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-06-13 20:26:06 +0200

    Finish GetUserEntriesFromToday

--- KcalCalcRest/Controllers/HistoryController.cs
+++ KcalCalcRest/Controllers/HistoryController.cs
@@ -18,7 +18,7 @@ public class HistoryController : BaseApiController {
 	
 	[Authorize(AuthenticationSchemes = "Bearer")]
 	[HttpGet("history-per-day")]
-	public async Task<IActionResult> GetUserEntriesFromToday() {
+	public async Task<IActionResult> GetUserEntriesFromToday() { // TODO: test if this returns correct values of sums
 		var username = _contextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name);
 		if (username is null) {
 			return BadRequest("Couldn't get username from context.");
@@ -28,6 +28,14 @@ public class HistoryController : BaseApiController {
 			return BadRequest("User not found.");
 		}
 		
-		var productEntries = await _repository.ProductEntries.Get(user.Id);
+		var productEntriesByDate = await _repository.ProductEntries.GetEntriesGroupedByDate(user.Id);
+		var toReturn = productEntriesByDate.Select(g => new {
+			Date = g.Key,
+			Kcal = g.Sum(e => e.Product.Kcal),
+			Protein = g.Sum(e => e.Product.Protein),
+			Carbohydrate = g.Sum(e => e.Product.Carbohydrate),
+			Fat = g.Sum(e => e.Product.Fat),
+		});
+		return Ok(toReturn);
 	}
 }
