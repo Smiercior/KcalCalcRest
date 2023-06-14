ab327f01b144e19649ee133c9930f4447d825eb3
Seweryn Presnal <IhateTrains@users.noreply.github.com>
2023-06-14 10:09:54 +0200

    daily-summary endpoint

--- KcalCalcRest/Controllers/HistoryController.cs
+++ KcalCalcRest/Controllers/HistoryController.cs
@@ -37,5 +37,29 @@ public class HistoryController : BaseApiController {
 		return Ok(toReturn);
 	}
 	
-	// TODO: product summmary
+
+	[HttpGet("daily-summary")]
+	public async Task<IActionResult> GetDailySummary() {
+		var username = _contextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name);
+		if (username is null) {
+			return BadRequest("Couldn't get username from context.");
+		}
+		var user = await _repository.UserAuthentication.GetUserAsync(username);
+		if (user is null) {
+			return BadRequest("User not found.");
+		}
+		
+		var productEntriesToday = await _repository.ProductEntries.GetAllUserEntriesToday(user.Id);
+		var toReturn = new {
+			Kcal = productEntriesToday.Sum(e => e.Product.Kcal),
+			Protein = productEntriesToday.Sum(e => e.Product.Protein),
+			Carbohydrate = productEntriesToday.Sum(e => e.Product.Carbohydrate),
+			Fat = productEntriesToday.Sum(e => e.Product.Fat),
+			user.KcalLimit,
+			user.ProteinLimit,
+			user.CarbohydrateLimit,
+			user.FatLimit
+		};
+		return Ok(toReturn);
+	}
 }
